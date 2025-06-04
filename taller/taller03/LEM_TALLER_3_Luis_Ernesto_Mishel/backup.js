606f6eff7d857b36c08b3939374855751cc6a77b
LushoCallapa <luiscallapa2004@gmail.com>
2025-06-04 15:22:50 -0400

    Taller3/LEM_Luis_Ernesto_Mishel

--- taller/taller03/LEM_TALLER_3_Luis_Ernesto_Mishel/backup.js
+++ taller/taller03/LEM_TALLER_3_Luis_Ernesto_Mishel/backup.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 require('dotenv').config();
 
 const job = new cron.CronJob(
-	'*/5 * * * *',
+	'0 0 * * *',
 	function () {
 		console.log('You will see this message every second');
         const dockerUser = 'postgres';
@@ -33,36 +33,6 @@ const job = new cron.CronJob(
 	true,
 );
 
-const cleanupJob = new cron.CronJob(
-    '0 0 */2 * *',
-    async function () {
-        console.log('Starting cleanup of old backups');
-        const folder = './backup';
-        try {
-            const files = await fs.readdir(folder);
-            const twoDaysAgo = new Date();
-            twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
-
-            for (const file of files) {
-                if (file.endsWith('.dump')) {
-                    const filePath = `${folder}/${file}`;
-                    const stats = await fs.stat(filePath);
-                    if (stats.mtime < twoDaysAgo) {
-                        await fs.unlink(filePath);
-                        console.log(`Deleted old backup: ${file}`);
-                    }
-                }
-            }
-            console.log('Cleanup completed');
-        } catch (error) {
-            console.error(`Error during cleanup: ${error.message}`);
-        }
-    },
-    null,
-    true
-);
-
 
 job.start();
-cleanupJob.start();
 console.log('Cron job started. It will log a message every second.');
